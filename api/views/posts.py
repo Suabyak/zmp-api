5bcfb7efdba40ac750304da1f5ac89a1fd9bc18f
Arkadiusz Torba <suabyak@gmail.com>
2023-05-14 12:36:57 +0200

    error code change

--- api/views/posts.py
+++ api/views/posts.py
@@ -11,10 +11,10 @@ class CreatePostView(APIView):
             user = get_user_from_token(request.META.get("HTTP_AUTHORIZATION"))
         except KeyError:
             return Response({
-                "message":"Token not provided"}, status=531)
+                "message":"Token not provided"}, status=530)
         except WrongTokenException:
             return Response({
-                "message":"Wrong token"}, status=532)
+                "message":"Wrong token"}, status=530)
         user = User.objects.filter(id=user["user_id"]).first()
         
         post = Post(
@@ -63,7 +63,7 @@ class UpdatePostView(APIView):
                 "message":"Token not provided"}, status=531)
         except WrongTokenException:
             return Response({
-                "message":"Wrong token"}, status=532)
+                "message":"Wrong token"}, status=530)
             
         post = Post.objects.filter(id=post_id).first()
         
@@ -73,7 +73,7 @@ class UpdatePostView(APIView):
             }, status=533)
             
         if post.user.id != user["user_id"]:
-            return Response(status=534)
+            return Response(status=530)
             
         post.body = request.data["body"]
         post.file = request.data["file"]
@@ -87,10 +87,10 @@ class DeletePostView(APIView):
             user = get_user_from_token(request.META.get("HTTP_AUTHORIZATION"))
         except KeyError:
             return Response({
-                "message":"Token not provided"}, status=531)
+                "message":"Token not provided"}, status=530)
         except WrongTokenException:
             return Response({
-                "message":"Wrong token"}, status=532)
+                "message":"Wrong token"}, status=530)
             
         post = Post.objects.filter(id=post_id).first()
         
@@ -113,10 +113,10 @@ class LikePostView(APIView):
             user = get_user_from_token(request.META.get("HTTP_AUTHORIZATION"))
         except KeyError:
             return Response({
-                "message":"Token not provided"}, status=531)
+                "message":"Token not provided"}, status=530)
         except WrongTokenException:
             return Response({
-                "message":"Wrong token"}, status=532)
+                "message":"Wrong token"}, status=530)
             
         post = Post.objects.filter(id=post_id).first()
         
@@ -127,7 +127,7 @@ class LikePostView(APIView):
             
             
         if post.user.id != user["user_id"]:
-            return Response(status=534)
+            return Response(status=530)
         
         likes = Likes.objects.filter(user_id=user["user_id"], post_id=post.id).first()
         if likes is not None:
@@ -149,10 +149,10 @@ class CommentPostView(APIView):
             user = get_user_from_token(request.META.get("HTTP_AUTHORIZATION"))
         except KeyError:
             return Response({
-                "message":"Token not provided"}, status=531)
+                "message":"Token not provided"}, status=530)
         except WrongTokenException:
             return Response({
-                "message":"Wrong token"}, status=532)
+                "message":"Wrong token"}, status=530)
             
         post = Post.objects.filter(id=post_id).first()
         
@@ -177,10 +177,10 @@ class GetFeedView(APIView):
             user = get_user_from_token(request.META.get("HTTP_AUTHORIZATION"))
         except KeyError:
             return Response({
-                "message":"Token not provided"}, status=531)
+                "message":"Token not provided"}, status=530)
         except WrongTokenException:
             return Response({
-                "message":"Wrong token"}, status=532)
+                "message":"Wrong token"}, status=530)
         
         observations = Observation.objects.filter(user=user["user_id"])
         observations = serialize_model_list(observations)
