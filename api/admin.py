d21edffd80e2a45de897626feabc12c83b77b197
Arkadiusz Torba <suabyak@gmail.com>
2023-05-14 10:34:54 +0200

    admin models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+class PostAdmin(Post):
+    pass
+
+admin.site.register(Post, PostAdmin)
+
+class LikesAdmin(Likes):
+    pass
+
+admin.site.register(Likes, LikesAdmin)
+
+class CommentAdmin(Comment):
+    pass
+
+admin.site.register(Comment, CommentAdmin)
+
+class ObservationAdmin(Observation):
+    pass
+
+admin.site.register(Observation, ObservationAdmin)
