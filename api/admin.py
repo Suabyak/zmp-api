356dfadc5ca5199f8024579d67480e5c8114c3e9
Arkadiusz Torba <suabyak@gmail.com>
2023-05-14 12:07:23 +0200

    admin fix

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Post, PostAdmin)
+
+class LikesAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Likes, LikesAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Comment, CommentAdmin)
+
+class ObservationAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Observation, ObservationAdmin)
 
 # Register your models here.
